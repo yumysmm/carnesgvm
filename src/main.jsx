04e6bf6ee1e699605f6aee10e344693478ef0668
Mutiservicios.sym <www.manuel.murillo@gmail.com>
2026-08-16 22:19:44 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import AdminApp from "./admin/AdminApp.jsx";
 import "./styles.css";
+import "./admin/admin.css";
+
+function Root() {
+  const [isAdmin, setIsAdmin] = useState(window.location.hash.startsWith("#admin"));
+
+  useEffect(() => {
+    const onHashChange = () => setIsAdmin(window.location.hash.startsWith("#admin"));
+    window.addEventListener("hashchange", onHashChange);
+    return () => window.removeEventListener("hashchange", onHashChange);
+  }, []);
+
+  return isAdmin ? <AdminApp /> : <App />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
